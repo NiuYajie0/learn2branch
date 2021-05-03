8736bc8b7d50a761995873832c91a3e19433026a
Chen Peng <pz0513@gmail.com>
2021-05-04 01:33:06 +0800

    Update setup.py
    Merged upstream

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
+<<<<<<< HEAD
 from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize("ml_ext_pyscipopt.pyx"),
 )
+=======
+from setuptools import setup, find_packages
+
+
+setup(
+    name="learn2branch",
+    version="1.0.0",
+    author="Maxime Gasse, Didier Chételat, Nicola Ferroni, Laurent Charlin, Andrea Lodi",
+    install_requires=["numpy", "scipy"],
+    packages=["learn2branch"],
+    package_dir={"learn2branch": "."},
+)
+>>>>>>> upstream/master
