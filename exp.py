40b864fc305e4c8d08a7ddfe5acd2914c7be603a
pz0513 <pz0513@gmail.com>
2021-06-17 11:02:00 +0800

    fixes

--- exp.py
+++ exp.py
@@ -10,37 +10,44 @@ if __name__ == '__main__':
 
     problem = "setcover"
     samplingStrategy = "uniform5" # choices: uniform5, depthK
-    sample_seed = 1
+    sampling_seed = 1
     train_seeds = "range(0,10)"
     gpu = 0 # CUDA GPU id (-1 for CPU).
 
-    #%%
-    # S01_args = SimpleNamespace()
-    # S01_args.problem = problem
-    # S01_args.n_instances = "(100, 20, 10, 20)" # python expression as string, to be used with eval(...) # Number of generated instances as (n_train_instances, n_valid_instances, n_transfer_instances, n_test_instances).
-    # S01_generate_instances.exp_main(S01_args)
+    # %%
+    S01_args = {
+        'problem' : problem,
+        'n_instances' : "(100, 20, 10, 20)",
+        'seed' : 0,
+    }
+    S01_args = SimpleNamespace(**S01_args)
+    S01_generate_instances.exp_main(S01_args)
 
 
     # strategies = ["uniform5", "depthK"]
     # for samplingStrategy in strategies:
 
-    #%%
-    S02_args = SimpleNamespace()
-    S02_args.problem = problem
-    S02_args.sampling = samplingStrategy
-    S02_args.seed = sample_seed
-    S02_args.njobs = 9
-    S02_args.n_samples = "(1000, 200, 200)" # Number of generated n_samples as (train_size, valid_size, test_size).
+    # %%
+    S02_args = {
+        'problem' : problem,
+        'sampling' : samplingStrategy,
+        'seed' : sampling_seed,
+        'njobs' : 9,
+        'n_samples' : "(1000, 200, 200)" # Number of generated n_samples as (train_size, valid_size, test_size).
+    }
+    S02_args = SimpleNamespace(**S02_args)
     S02_generate_dataset.exp_main(S02_args)
 
     # %%
-    S03_args = SimpleNamespace()
-    S03_args.model = 'baseline'
-    S03_args.gpu = gpu
-    S03_args.problem = problem
-    S03_args.sampling = samplingStrategy
-    S03_args.sample_seed = sample_seed
-    S03_args.seeds = train_seeds # python expression as string, to be used with eval(...)
+    S03_args = {
+        'model' : 'baseline',
+        'gpu' : gpu,
+        'problem' : problem,
+        'sampling' : samplingStrategy,
+        'sample_seed' : sampling_seed,
+        'seeds' : train_seeds # python expression as string, to be used with eval(...)
+    }
+    S03_args = SimpleNamespace(**S03_args)
     S03_train_gcnn.exp_main(S03_args)
 
     # %%
@@ -48,12 +55,8 @@ if __name__ == '__main__':
         'gpu': gpu,
         'problem': problem,
         'sampling' : samplingStrategy,
-        'sample_seed' : sample_seed,
+        'sample_seed' : sampling_seed,
         'seeds' : train_seeds, # python expression as string, to be used with eval(...)
     }
     S04_args = SimpleNamespace(**S04_args)
-    # S04_args.problem = problem
-    # S04_args.sampling = samplingStrategy
-    # S04_args.sample_seed = sample_seed
-    # S04_args.seeds = train_seeds 
     S04_test.exp_main(S04_args)
