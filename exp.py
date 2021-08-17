7c03d1db581356a763893906add51d3c9d983706
Chen Peng <pz0513@gmail.com>
2021-08-18 02:43:40 +0800

    准备换成large dataset test

--- exp.py
+++ exp.py
@@ -15,32 +15,32 @@ if __name__ == '__main__':
     gpu = 0 # CUDA GPU id (-1 for CPU).
 
     # %%
-    ##  01 - Generate instances
-    # S01_args = {
-    #     'problem' : problem,
-    #     'n_instances' : "(100, 20, 20, 20)",
-    #     'seed' : 0,
-    # }
-    # S01_args = SimpleNamespace(**S01_args)
-    # S01_generate_instances.exp_main(S01_args)
+    #  01 - Generate instances
+    S01_args = {
+        'problem' : problem,
+        'n_instances' : "(100, 20, 20, 20)",
+        'seed' : 0,
+    }
+    S01_args = SimpleNamespace(**S01_args)
+    S01_generate_instances.exp_main(S01_args)
 
     
     samplingStrategies = ['uniform5', 'depthK', 'depthK2'] # # choices: 'uniform5', 'depthK', 'depthK2', 'depthK_adaptive'
     sampling_seed = 0
     for samplingStrategy in samplingStrategies:
 
-        # # %%
-        # # 02 - Collect training samples
-        # S02_args = {
-        #     'problem' : problem,
-        #     'sampling' : samplingStrategy,
-        #     'seed' : sampling_seed,
-        #     'njobs' : 7,
-        #     'n_samples' : "(1000, 200, 200)" # Number of generated n_samples as (train_size, valid_size, test_size).
-        #     #             "(1000, 200, 200)"
-        # }
-        # S02_args = SimpleNamespace(**S02_args)
-        # S02_generate_dataset.exp_main(S02_args)
+        # %%
+        # 02 - Collect training samples
+        S02_args = {
+            'problem' : problem,
+            'sampling' : samplingStrategy,
+            'seed' : sampling_seed,
+            'njobs' : 7,
+            'n_samples' : "(1000, 200, 200)" # Number of generated n_samples as (train_size, valid_size, test_size).
+            #             "(1000, 200, 200)"
+        }
+        S02_args = SimpleNamespace(**S02_args)
+        S02_generate_dataset.exp_main(S02_args)
 
         # # %%
         # ## 03 - Train GCNN
@@ -57,12 +57,12 @@ if __name__ == '__main__':
 
         # # %%
         # ### 04 - Test branching accuracies w.r.t. strong branching
-        S04_args = {
-            'gpu': gpu,
-            'problem': problem,
-            'sampling' : samplingStrategy,
-            'sample_seed' : sampling_seed,
-            'seeds' : train_seeds, # python expression as string, to be used with eval(...)
-        }
-        S04_args = SimpleNamespace(**S04_args)
-        S04_test.exp_main(S04_args)
+        # S04_args = {
+        #     'gpu': gpu,
+        #     'problem': problem,
+        #     'sampling' : samplingStrategy,
+        #     'sample_seed' : sampling_seed,
+        #     'seeds' : train_seeds, # python expression as string, to be used with eval(...)
+        # }
+        # S04_args = SimpleNamespace(**S04_args)
+        # S04_test.exp_main(S04_args)
